1353cc2591ca66f12be4f04e6ee4f22239575b9d
Josh Boss <jsb91@duke.edu>
2021-08-19 13:00:41 -0400

    another attempt

--- src/index.js
+++ src/index.js
@@ -3,17 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./js/App";
 
-(function(l) {
-  if (l.search[1] === '/' ) {
-    var decoded = l.search.slice(1).split('&').map(function(s) { 
-      return s.replace(/~and~/g, '&')
-    }).join('?');
-    window.history.replaceState(null, null,
-        l.pathname.slice(0, -1) + decoded + l.hash
-    );
-  }
-}(window.location));
-
 ReactDOM.render(
   <BrowserRouter>
     <App />
